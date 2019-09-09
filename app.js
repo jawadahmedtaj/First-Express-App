632664f250495016294e1e3b740ba367b746e638
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-09 09:18:44 +0200

    request tests

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ app.get("/", function (req, res) {
     res.send("Hi there!");
 })
 
+
+app.get("/bye", function (req, res) {
+    res.send("Bye!");
+})
+
+
+app.get("/dog", function (req, res) {
+    console.log("Someone made a request to /dog");
+    res.send("Meow!");
+})
+
 //process.env.PORT, process.env.IP
 app.listen(3000, function () {
     console.log("Listening on port 3000");
